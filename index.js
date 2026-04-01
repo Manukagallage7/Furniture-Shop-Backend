1a30841e3e626df7267a57c50cce94774c24f473
Manukagallage7 <manukakavinda1110@gmail.com>
2026-04-01 23:00:20 +0530

    Refactor database connection to use environment variable and organize imports

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+
+dotenv.config();
 
 const app = express()
 
 app.use(bodyParser.json())
 
-const connectionString = "mongodb+srv://manukakavinda1110_db_user:Furniture123@cluster0.cdvzhzo.mongodb.net/?appName=Cluster0"
+const connectionString = process.env.MDB_URI
 
 mongoose.connect(connectionString).then(()=>{
     console.log("Database Connction Successfully")
 }).catch((err)=>{
     console.log("Database Connection Failed", err)
 })
+
+app.use("/users", userRouter)
 
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000")
