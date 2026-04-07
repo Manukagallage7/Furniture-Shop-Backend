6c9af43bfb0a7056ea97aeb3eb5aab26e6c625a7
Manukagallage7 <manukakavinda1110@gmail.com>
2026-04-07 15:43:38 +0530

    Add CORS middleware to enable cross-origin resource sharing

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import jwt from "jsonwebtoken";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(cors());
 
 app.use((req, res, next) => {
     const value = req.header("Authorization")
