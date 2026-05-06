aee1701b3643d7b8f68c04cbc4e59b20b246ab6f
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-07 01:21:37 +0530

    Fix route for retrieving product information by updating endpoint path

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -7,6 +7,6 @@ productRouter.post("/create", createProduct)
 productRouter.put("/update/:productId", updateProduct)
 productRouter.delete("/delete/:productId", deleteProduct)
 productRouter.get("/get", getProduct)
-productRouter.get("/getInfo/:productId", getProductInfo)
+productRouter.get("/get/:productId", getProductInfo)
 
 export default productRouter;
