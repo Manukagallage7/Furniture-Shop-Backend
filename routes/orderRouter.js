04c8dd552860598dcac9c2007ef1533cb4fde285
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-08 17:50:49 +0530

    Add getOrders route to orderRouter for retrieving orders

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createOrder } from '../controllers/orderController.js';
+import { createOrder, getOrders } from '../controllers/orderController.js';
 
 const orderRouter = express.Router()
 
 orderRouter.post("/create", createOrder)
+orderRouter.get("/get", getOrders)
 
 export default orderRouter
