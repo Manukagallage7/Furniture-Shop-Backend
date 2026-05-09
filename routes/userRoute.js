559811cdcef959c618a86341ea7aad38bf959cd5
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-09 10:27:49 +0530

    Add getUser route to userRouter for retrieving user details

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, loginUser, getUser } from "../controllers/userController.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/register", createUser)
 userRouter.post("/login", loginUser)
+userRouter.get("/get", getUser)
 
 export default userRouter;
