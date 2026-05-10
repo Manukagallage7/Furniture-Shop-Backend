46a74812ccc40e711374fc7b81e44556e033ae89
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-10 11:28:17 +0530

    Add updateOrder function to orderRouter for updating existing orders

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createOrder, getOrders, getOrderById } from '../controllers/orderController.js';
+import { createOrder, getOrders, updateOrder, getOrderById  } from '../controllers/orderController.js';
 
 const orderRouter = express.Router()
 
 orderRouter.post("/create", createOrder)
 orderRouter.get("/get/:page/:limit", getOrders)
 orderRouter.get("/get/:orderId", getOrderById)
+orderRouter.put("/update/:orderId", updateOrder)
 
 export default orderRouter
