3abc7c4586e4dbfc410c86651423735430478432
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-10 11:28:22 +0530

    Add getUsers route to userRouter for retrieving paginated user list

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser, loginUser, getUser } from "../controllers/userController.js";
+import { createUser, loginUser, getUser, getUsers } from "../controllers/userController.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/register", createUser)
 userRouter.post("/login", loginUser)
 userRouter.get("/get", getUser)
+userRouter.get("/users/:page/:limit", getUsers)
 
 export default userRouter;
