bfe694bd55da365eaeeca2d25b405e5d33ba695f
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-12 16:47:20 +0530

    Add user management routes for email-based operations and Google login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { createUser, loginUser, getUser, getUsers } from "../controllers/userController.js";
+import { createUser, loginUser, getUser, getUsers, googleLogin, logOutUser, updateUser, deleteUser, blockUser, unblockUser, getUserByEmail, updateUserByEmail } from "../controllers/userController.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/register", createUser)
 userRouter.post("/login", loginUser)
 userRouter.get("/get", getUser)
+userRouter.get("/:email", getUserByEmail)
+userRouter.put("/:email", updateUserByEmail)
 userRouter.get("/users/:page/:limit", getUsers)
+userRouter.post("/google-login", googleLogin)
+userRouter.put("/update", updateUser)
+userRouter.delete("/delete", deleteUser)
+userRouter.put("/blockUser/:id", blockUser)
+userRouter.put("/unblockUser/:id", unblockUser)
+userRouter.post("/logout", logOutUser)
 
 export default userRouter;
