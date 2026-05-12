8e3a475f724eee2b7991e66700393e87a8002cb2
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-13 00:09:48 +0530

    Add search and category retrieval routes to product API

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, updateProduct, deleteProduct, getProduct, getProductInfo } from "../controllers/productController.js";
+import { createProduct, updateProduct, deleteProduct, getProduct, getProductInfo,searchProducts, getProductsByCategory } from "../controllers/productController.js";
 
 const productRouter = express.Router()
 
@@ -8,5 +8,7 @@ productRouter.put("/update/:productId", updateProduct)
 productRouter.delete("/delete/:productId", deleteProduct)
 productRouter.get("/get", getProduct)
 productRouter.get("/get/:productId", getProductInfo)
+productRouter.get("/search/:query", searchProducts)
+productRouter.get("/category/:category", getProductsByCategory)
 
 export default productRouter;
