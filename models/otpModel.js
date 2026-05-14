8bf20ad5349ddc9b33c2c4241db8ef3b3f84357a
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-14 09:59:41 +0530

    Add OTP model for storing email and OTP data

---
+++ models/otpModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const otpSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            required: true,
+        },
+        otp: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            required: true,
+        },
+});
+
+const OTPModel = mongoose.model('OTP', otpSchema);
+export default OTPModel;
